65e52f13f95e7fc971632f1182c5c6eee04f3ab5
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 16:00:07 -0300

    Preparing DatabaseFatory and organizing MongoStrategy to receive mongodb instace.

--- databases/strategy/MongoStrategy.py
+++ databases/strategy/MongoStrategy.py
@@ -2,4 +2,9 @@ from adapters.NoRelationalAdapter import NoRelationalAdapter
 
 
 class MongoStrategy(NoRelationalAdapter):
-    pass
+    def __init__(self, mongodb_instance):
+        self._mongodb = mongodb_instance
+
+    @property
+    def mongodb(self):
+        return self._mongodb
