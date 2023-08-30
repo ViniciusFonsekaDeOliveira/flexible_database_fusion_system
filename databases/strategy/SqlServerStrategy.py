9baa9416086e950b72b720be80d1532fcfc3aa50
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 16:41:15 -0300

    Preparing SQL Server Strategy to receive SQLServer instance

--- databases/strategy/SqlServerStrategy.py
+++ databases/strategy/SqlServerStrategy.py
@@ -2,4 +2,11 @@ from adapters.RelationalAdapter import RelationalAdapter
 
 
 class SQLServerStrategy(RelationalAdapter):
-    pass
+    def __init__(self, sqlserver_instance):
+        self._sqlserver = sqlserver_instance
+
+    @property
+    def sqlserver(self):
+        return self._sqlserver
+
+   # TO DO: especific CRUD operations related to SQL Server database
