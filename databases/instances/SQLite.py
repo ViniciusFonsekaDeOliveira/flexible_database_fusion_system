351166acce7be9f52522f52db1d18c77d69de6ec
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-29 21:41:27 -0300

    Preparing SQLite instance

--- databases/instances/SQLite.py
+++ databases/instances/SQLite.py
@@ -2,4 +2,15 @@ from databases.connection.SqliteDatabase import SQLiteDatabase
 
 
 class SQLite:
-    pass
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SQLite, cls).__new__(cls)
+            cls._instance.db_instance = SQLiteDatabase(
+                host=None,
+                port=None,
+                user=None,
+                password=None,
+                database_name="mySqlite_db.db"
+            )
