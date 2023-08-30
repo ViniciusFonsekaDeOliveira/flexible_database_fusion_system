6701e4bc2565ebb822176c41d3181d789b50c851
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-29 21:50:02 -0300

    Preparing SQLServer instance

--- databases/instances/SQLServer.py
+++ databases/instances/SQLServer.py
@@ -2,4 +2,16 @@ from databases.connection.SQLServerDatabase import SQLServerDatabase
 
 
 class SQLServer:
-    pass
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SQLServer, cls).__new__(cls)
+            cls._instance.db_instance = SQLServerDatabase(
+                host="localhost",
+                port=1433,
+                user="user",
+                password="password",
+                database_name="mySQLServer_db"
+            )
+        return cls._instance
