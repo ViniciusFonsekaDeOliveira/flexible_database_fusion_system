b8dc3db2e1009b0ba1abab23dae5b3c84b90c121
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-29 22:44:14 -0300

    Preparing Person Repository to be flexible to database strategy

--- repositories/PersonRepository.py
+++ repositories/PersonRepository.py
@@ -1,2 +1,23 @@
 class PersonRepository:
-    pass
+    def __init__(self, database_strategy) -> None:
+        self._strategy = database_strategy
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, database_strategy):
+        self._strategy = database_strategy
+
+    def find_person_by_id(self):
+        pass
+
+    def insert_person(self):
+        pass
+
+    def update_person(self):
+        pass
+
+    def delete_person(self):
+        pass
