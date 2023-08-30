d68906212f9b42bedc385c9e7b70d2d29ccf73ae
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 16:37:50 -0300

    Preparing Sqlite Strategy to receive sqlite instance

--- databases/strategy/SQLiteStrategy.py
+++ databases/strategy/SQLiteStrategy.py
@@ -2,4 +2,11 @@ from adapters.RelationalAdapter import RelationalAdapter
 
 
 class SQLiteStrategy(RelationalAdapter):
-    pass
+    def __init__(self, sqlitedb_instance):
+        self._sqlitedb = sqlitedb_instance
+
+    @property
+    def sqlitedb(self):
+        return self._sqlitedb
+
+    # TO DO: especific CRUD operations realted to sqlite database
