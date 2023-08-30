69cc4e8d3b029d4e2d9befbbad7a076347f0c7bc
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-29 21:33:20 -0300

    Preparing Postgres Instance

--- databases/instances/Postgres.py
+++ databases/instances/Postgres.py
@@ -2,4 +2,16 @@ from databases.connection.PostgresDatabase import PostgresDatabase
 
 
 class Postgres:
-    pass
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Postgres, cls).__new__(cls)
+            cls._instance.db_instance = PostgresDatabase(
+                host="localhost",
+                port=5432,
+                user="postgres",
+                password="example",
+                database_name="myPostgres_db"
+            )
+        return cls._instance
