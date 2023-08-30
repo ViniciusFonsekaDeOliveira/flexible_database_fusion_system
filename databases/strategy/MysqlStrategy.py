a644d48242d609dae7d96478b0642482ca1fbda1
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 16:11:30 -0300

    Preparing MySQL Strategy to receive mysql instance

--- databases/strategy/MysqlStrategy.py
+++ databases/strategy/MysqlStrategy.py
@@ -2,4 +2,11 @@ from adapters.RelationalAdapter import RelationalAdapter
 
 
 class MySQLStrategy(RelationalAdapter):
-    pass
+    def __init__(self, mysqldb_instace):
+        self._mysqldb = mysqldb_instace
+
+    @property
+    def mysqldb(self):
+        return self._mysqldb
+
+    # TO DO: especific CRUD operations to mysql databases
