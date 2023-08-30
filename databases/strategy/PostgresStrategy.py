f3244e99ab0cba9cfeab08cb25e77a7737f32e0f
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 16:34:49 -0300

    Preparing Postgres Strategy to receive postgres instance

--- databases/strategy/PostgresStrategy.py
+++ databases/strategy/PostgresStrategy.py
@@ -2,4 +2,11 @@ from adapters.RelationalAdapter import RelationalAdapter
 
 
 class PostgresStrategy(RelationalAdapter):
-    pass
+    def __init__(self, postgresdb_instance):
+        self._postgresdb = postgresdb_instance
+
+    @property
+    def postgresdb(self):
+        return self._postgresdb
+
+   # TO DO: especific CRUD operations related to postgres database
