d6deaed5d3a712924aad5bea294127fafcf1525f
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-08-30 21:40:22 -0300

    Feat: Database Factoring

--- databases/factory/DatabaseFactory.py
+++ databases/factory/DatabaseFactory.py
@@ -1,2 +1,22 @@
+from instances.SQLite import SQLite
+from instances.MySQL import MySQL
+from instances.Postgres import Postgres
+from instances.SQLServer import SQLServer
+from instances.Mongo import Mongo
+
+
 class DatabaseFactory:
-    pass
+    @staticmethod
+    def create_database_connection(self, db_type: str, **kwargs):
+        if db_type == "sqlite":
+            return SQLite(**kwargs)
+        elif db_type == "mysql":
+            return MySQL(**kwargs)
+        elif db_type == "postgres":
+            return Postgres(**kwargs)
+        elif db_type == "sqlserver":
+            return SQLServer(**kwargs)
+        elif db_type == "mongodb":
+            return Mongo(**kwargs)
+        else:
+            print("Invalid database option!")
